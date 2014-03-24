51715fd30b572cbf72d1501f2596b20da4e72834
kylenewsome <kyle@bitwit.ca>
2014-03-24 14:38:34 -0400

    adjusting videojs start up settings moving postcard scripts to footer

--- postcard-core/templates/post-template-custom-default.php
+++ postcard-core/templates/post-template-custom-default.php
@@ -4,7 +4,7 @@ echo nl2br(stripslashes(postcard_style_post_message($postcard["message"]))); ?>
 if (isset($postcard["url"])):?>
 <a target="_blank" class="postcard-link" href="<?php echo $postcard["url"] ?>">Visit Link</a><?php endif;
 if (isset($postcard["video"])): ?>
-<video id="postcard-video-<?php echo $postcard["id"]; ?>" poster="<?php echo $postcard["image"]; ?>" width="<?php echo $postcard["width"]; ?>" height="<?php echo $postcard["height"]; ?>" class="video-js vjs-default-skin" data-setup='{}' controls loop preload="auto">
+<video id="postcard-video-<?php echo $postcard["id"]; ?>" poster="<?php echo $postcard["image"]; ?>" width="<?php echo $postcard["width"]; ?>" height="<?php echo $postcard["height"]; ?>" class="video-js vjs-default-skin" controls loop preload="auto">
     <source src="<?php echo $postcard["video"]; ?>" type="video/mp4">
 </video><?php
 elseif (isset($postcard["image"])): ?>
